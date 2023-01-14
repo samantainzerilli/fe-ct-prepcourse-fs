04adb18a87e9c3292511347dd1d90b645917ef27
Samanta Inzerilli <samantainzerilli@gmail.com>
2023-01-14 15:09:29 +0100

    homework JS04

---
+++ bucles.js
@@ -0,0 +1,13 @@
+var suma=0
+//suma=suma+1;
+//console.log (suma);
+
+//for(var i=0 ; i<10; i++) {
+  //  suma=suma +1;
+   // console.log
+//}
+
+while(suma<3){
+    suma= suma +1;
+    console.log (suma);
+}
---
+++ operadoreslogicos.js
@@ -0,0 +1,10 @@
+//function MayorYMenorYPar(numero){
+  //  if (numero>5 && numero<10 && numero%2===0) console.log (true);
+    //else console.log (false);
+//}
+//MayorYMenorYPar(3)
+
+//function OperadorOR(str){
+  //  if(str === 'Henry' || str.length<2 ) console.log (true);
+   // else console.log(false);
+//}
